cd6f9439d779ff619f8e103a6caaeb968d1087cf
andi107 <andifirman107@gmail.com>
2022-09-08 18:06:07 +0700

    Update

--- frontend/resources/views/components/base-layout.blade.php
+++ frontend/resources/views/components/base-layout.blade.php
@@ -1,29 +1,41 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	{{-- <title>Home 01</title> --}}
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-<!--===============================================================================================-->	
-	<link rel="icon" type="image/png" href="{{ asset('images/icons/favicon.png')}}"/>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="image/png" href="{{ asset('src/images/icons/favicon.png')}}" />
     <x-corecss-component />
     {{ $titleSlot }}
+
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-XB7PG0P5TG"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'G-XB7PG0P5TG');
+
+    </script>
 </head>
 <body class="animsition">
-	<!-- Header -->
-	<x-header-page-component />
-	<!-- Content -->
-	{{ $slot }}
-	<!-- Footer -->
-	<x-footer-page-component />
-	{{ $cssPage ?? '' }}
-	<!-- Back to top -->
-	<div class="btn-back-to-top" id="myBtn">
-		<span class="symbol-btn-back-to-top">
-			<span class="fas fa-angle-up"></span>
-		</span>
-	</div>
+    <!-- Header -->
+    <x-header-page-component />
+    <!-- Content -->
+    {{ $slot }}
+    <!-- Footer -->
+    <x-footer-page-component />
+    {{ $cssPage ?? '' }}
+    <!-- Back to top -->
+    <div class="btn-back-to-top" id="myBtn">
+        <span class="symbol-btn-back-to-top">
+            <span class="fas fa-angle-up"></span>
+        </span>
+    </div>
     <x-corejs-component />
-	{{ $jsPage ?? '' }}
+    {{ $jsPage ?? '' }}
 </body>
 </html>
