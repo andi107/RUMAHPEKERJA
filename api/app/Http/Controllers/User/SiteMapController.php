6588678ffaa2539842e09e1a94c2990cc7e4d761
andi107 <andifirman107@gmail.com>
2022-10-07 19:11:02 +0700

    Update

--- api/app/Http/Controllers/User/SiteMapController.php
+++ api/app/Http/Controllers/User/SiteMapController.php
@@ -8,10 +8,20 @@ use Carbon\Carbon;
 class SiteMapController extends Controller {
 
     public function st_posts() {
-        $data = DB::table('sitemap_posts')
-        ->where('fnstatus','=',1)
-        ->orderBy('updated_at','desc')
-        ->get();
+        // $data = DB::table('sitemap_posts')
+        // ->where('fnstatus','=',1)
+        // ->orderBy('updated_at','desc')
+        // ->get();
+        $data = DB::select("select
+            a.*,ctg.ftname as ftcategory_name
+            from (
+                select
+                    pst.fttitle, pst.ftdescription ,pst.ftuniq ,pst.fncategory,pst.fttitle_url,pst.created_at , pst.updated_at,pst.fnstatus,
+                    gal.ftname as ftgalery_name,gal.ftfolder as ftgalery_folder,gal.ftext as ftgalery_ext
+                from posts pst left join galery gal
+                    on (pst.id = gal.fncontent_id and gal.fttype = 'baner')
+            ) a left join category ctg
+            on (a.fncategory = ctg.id)");
         return response()->json([
             'data' => $data
         ], 200);
