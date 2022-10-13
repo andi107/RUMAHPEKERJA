ca676a4e485715147ad59e8a38b156963c70f7d8
andi107 <andifirman107@gmail.com>
2022-10-13 14:05:23 +0700

    Update Clarity Bing

--- frontend/resources/views/components/base-layout.blade.php
+++ frontend/resources/views/components/base-layout.blade.php
@@ -43,7 +43,10 @@
 
         </script>
         <script src="https://www.googleoptimize.com/optimize.js?id=OPT-5F792S9"></script>
-    @endif
+    
+
+<!-- Clarity tracking code for https://rumahpekerjahebat.com/ --><script>    (function(c,l,a,r,i,t,y){        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i+"?ref=bwt";        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);    })(window, document, "clarity", "script", "e299utxn9x");</script>
+@endif
 </head>
 <body class="animsition">
     
