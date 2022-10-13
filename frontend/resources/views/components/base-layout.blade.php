942eb7639c36263794031470b68835e50aeec627
andi107 <andifirman107@gmail.com>
2022-10-13 14:17:09 +0700

    update clarity

--- frontend/resources/views/components/base-layout.blade.php
+++ frontend/resources/views/components/base-layout.blade.php
@@ -60,7 +60,13 @@
     })(window, document, "clarity", "script", "e299utxn9x");
 </script>
 
-
+<script type="text/javascript">
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "e29pxggw2e");
+</script>
 @endif
 </head>
 <body class="animsition">
